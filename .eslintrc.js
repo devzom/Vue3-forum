dd8b210ea71cb4daca7b7c978239c6dfbff43997
Jakub Zomerfeld <dev.zomerfeld@gmail.com>
2021-04-18 01:46:09 +0200

    update:Lint options,.editorconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
+    node: true,
   },
+
   parserOptions: {
     parser: 'babel-eslint',
     sourceType: 'module',
   },
+
   extends: [
     'airbnb-base',
     'plugin:vue/vue3-essential',
     'prettier',
     'plugin:prettier/recommended',
+    '@vue/airbnb',
   ],
+
   rules: {
     'comma-dangle': 'off',
     'class-methods-use-this': 'off',
@@ -27,6 +31,16 @@ module.exports = {
         ignores: [],
       },
     ],
-    'prettier/prettier': ['error', { singleQuote: true, endOfLine: 'auto' }],
+
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
+
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
