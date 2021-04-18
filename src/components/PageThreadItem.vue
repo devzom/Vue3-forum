336b7d53fdbd75bfac7a4004f65d719e46e7e0f4
Jakub Zomerfeld <dev.zomerfeld@gmail.com>
2021-04-18 17:48:05 +0200

    add:PageHome, PageThread, ThreadList,

---
+++ src/components/PageThreadItem.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="thread" class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <post-list :posts="threadPosts" />
+    <post-editor :post-id="id" @addPost="submitPost" />
+  </div>
+
+  <div v-else class="col-full text-center center-center">
+    <h1>Thread doesn't exist</h1>
+    <router-link :to="{ name: 'Home' }">Read other cool threads</router-link>
+  </div>
+</template>
+
+<script>
+import jsonData from '@/assets/dummyData.json';
+import PostList from '@/components/PostList';
+import PostEditor from '@/components/PostEditor';
+
+export default {
+  name: 'PageThreadItem',
+  components: { PostEditor, PostList },
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      threads: jsonData.threads,
+      posts: jsonData.posts
+    };
+  },
+  computed: {
+    thread() {
+      return this.threads.find((thread) => thread.id === this.id);
+    },
+    threadPosts() {
+      return this.posts.filter((post) => post.threadId === this.id);
+    }
+  },
+  methods: {
+    submitPost(newPost) {
+      const post = {
+        ...newPost.post,
+        threadId: this.id
+      };
+
+      this.posts.push(post);
+      this.thread.posts.push(post.id);
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Vue forum</h1>
+  <page-thread-list :threads="threads" />
+</template>
+
+<script>
+import data from '@/assets/dummyData.json';
+import PageThreadList from '@/pages/PageThreadList';
+
+export default {
+  name: 'PageHome',
+  components: { PageThreadList },
+  data() {
+    return {
+      threads: data.threads
+    };
+  }
+};
+</script>
