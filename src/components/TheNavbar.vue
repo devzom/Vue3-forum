977ef6b93e62b1b50f523f18817f7261cd81361a
Jakub Zomerfeld <dev.zomerfeld@gmail.com>
2021-05-14 20:20:56 +0200

    feat:Add TheNavbar

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <header id="header" class="header">
+    <a href="index.html" class="logo">Vue Forum</a>
+
+    <div class="btn-hamburger">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+
+    <!-- use .navbar-open to open nav -->
+    <nav class="navbar">
+      <ul>
+        <li class="navbar-user">
+          <a href="#">
+            <img class="avatar-small" src="https://picsum.photos/50/50" alt="" />
+            <span>
+              My profile
+              <img class="icon-profile" src="../assets/img/svg/arrow-profile.svg" alt="" />
+            </span>
+          </a>
+
+          <!-- dropdown menu -->
+          <!-- add class "active-drop" to show the dropdown -->
+          <div id="user-dropdown">
+            <div class="triangle-drop"></div>
+            <ul class="dropdown-menu">
+              <li class="dropdown-menu-item"><a href="#">View profile</a></li>
+              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+
+      <ul>
+        <li class="navbar-item">
+          <router-link :to="{ name: 'Home' }">Home</router-link>
+        </li>
+        <!--        <li class="navbar-item">-->
+        <!--          <router-link :to="{ name: 'Category' }">Category</router-link>-->
+        <!--        </li>-->
+        <!--        <li class="navbar-item">-->
+        <!--          <router-link :to="{ name: 'Forum' }">Forum</router-link>-->
+        <!--        </li>-->
+        <!--        <li class="navbar-item">-->
+        <!--          <router-link :to="{ name: 'Thread' }">Thread</router-link>-->
+        <!--        </li>-->
+        <!-- Show these option only on mobile-->
+        <li class="navbar-item mobile-only">
+          <a href="#">My Profile</a>
+        </li>
+        <li class="navbar-item mobile-only">
+          <a href="#">Logout</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheNavbar'
+};
+</script>
+
+<style></style>
